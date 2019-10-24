9beeeecb0ad692b15a71c7e6156e80a63f17b457
Andri Suga <andrisuga@Andris-MacBook-Air.local>
2019-10-24 20:51:21 +0300

    first

---
+++ Array.Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] numbers = new int[101];
+			int sum = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				numbers[i] = 0 + i;
+				Console.WriteLine(numbers[i]);
+			}
+
+			foreach (var item in numbers)
+			{
+				sum += item;
+			}
+			Console.WriteLine($"Kõikide numbrite kogusumma on: {sum}");
+		}
+    }
+}
